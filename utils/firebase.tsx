c6e25961b1800c7f93bdfc949be5ae6f1561c5bc
Curtis Chen <curtischen1234@gmail.com>
2026-03-20 00:28:00 -0700

    Add env variables for firebase

--- utils/firebase.tsx
+++ utils/firebase.tsx
@@ -3,14 +3,32 @@ import { getAuth } from "firebase/auth"
 import { GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
+const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
+const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+const measurementId = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+if (
+  !apiKey ||
+  !authDomain ||
+  !projectId ||
+  !storageBucket ||
+  !messagingSenderId ||
+  !appId ||
+  !measurementId
+) {
+  throw new Error("Missing env variables for firebase config")
+}
 const firebaseConfig = {
-  apiKey: "AIzaSyDs8Us0eaxeVP8b0mD_sU2LT7pOYZpyRbs",
-  authDomain: "personal-2722f.firebaseapp.com",
-  projectId: "personal-2722f",
-  storageBucket: "personal-2722f.firebasestorage.app",
-  messagingSenderId: "438663539541",
-  appId: "1:438663539541:web:e57fe32e1adbdf47951ddc",
-  measurementId: "G-VX55R7P4FD",
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
 }
 
 const app = initializeApp(firebaseConfig)
